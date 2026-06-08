83966efff6d30afdba6522b19cd10b57b3f01b19
Hao Yang <132206571+hyhy987@users.noreply.github.com>
2026-06-08 20:40:54 +0800

    Time: 87 ms (76.97%) | Memory: 15.8 MB (21.25%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,38 +2,35 @@ class Solution(object):
     def minWindow(self, s, t):
         if t == "":
             return ""
-        hashT = {}
-        hashS = {}
-        l, r = 0, 0
-        res, resLen = [-1, -1], float('inf')
-        
+        l = 0
+        hashS, hashT = {}, {}
 
-        # Build hashMap for t first
+        # Build hashMap of need
         for c in t:
             hashT[c] = 1 + hashT.get(c, 0)
-
+        
         have, need = 0, len(hashT)
+        res, resLen = [-1, -1], float("inf")
 
-        # Sliding window
         for r in range(len(s)):
             c = s[r]
             hashS[c] = 1 + hashS.get(c, 0)
 
-            if c in hashT and hashS[c] == hashT[c]:
+            if s[r] in hashT and hashS[c] == hashT[c]:
                 have += 1
-
+            
             while have == need:
-                # Update result
-                if (r - l + 1 ) < resLen:
+                if (r - l + 1) < resLen:
                     res = [l, r]
                     resLen = r - l + 1
                 hashS[s[l]] -= 1
 
                 if s[l] in hashT and hashS[s[l]] < hashT[s[l]]:
                     have -= 1
+
                 l += 1
-                
-        if resLen != float("inf"):
+        if resLen < float("inf"):
             return s[res[0]: res[1] + 1]
         else:
-            return ""            
+            return ""
+                
