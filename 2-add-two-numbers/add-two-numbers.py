7e4425631d65e5dbe19dd142082e594b4fe3ae17
Hao Yang <132206571+hyhy987@users.noreply.github.com>
2026-07-24 21:16:18 +0800

    Time: 3 ms (65.28%) | Memory: 19.2 MB (95.82%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,44 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        stack1 = []
-        stack2 = []
 
-        while l1:
-            stack1.append(l1.val)
-            l1 = l1.next
-        
-        while l2:
-            stack2.append(l2.val)
-            l2 = l2.next
-        
-        s1 = ""
-        s2 = ""
-        
-        while stack1:
-            s1 += str(stack1.pop())
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        p1, p2 = l1, l2
+        newList = ListNode(0)
+        curr = newList 
+        while p1 or p2 or carry:
+            val1 = p1.val if p1 else 0
+            val2 = p2.val if p2 else 0
+            val = val1 + val2 + carry
+            carry = val // 10 
+            rem = val % 10 
+            curr.next = ListNode(rem)
+            curr = curr.next
+            p1 = p1.next if p1 else None
+            p2 = p2.next if p2 else None
 
-        while stack2:
-            s2 += str(stack2.pop())
-
-        num = int(s1) + int(s2)
-        res = ListNode()
-        strNum = str(num)
-        strNum = strNum[::-1]
-        if num == 0:
-            return ListNode(0)
-        head = None
-        for i in range(len(strNum)):
-            newNode = ListNode(int(strNum[i]))
-            if not head:
-                head = newNode
-                curr = head
-            else:
-                curr.next = newNode
-                curr = curr.next
-        
-        return head
+        return newList.next
+            
