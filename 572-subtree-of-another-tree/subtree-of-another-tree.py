5ab32602f824b8b09d11cd6f38dd68de6cd058cd
Hao Yang <132206571+hyhy987@users.noreply.github.com>
2026-07-29 19:33:39 +0800

    Time: 56 ms (6.21%) | Memory: 19.5 MB (63.51%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -1,28 +1,39 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSubtree(self, root, subRoot):
-        """
-        :type root: Optional[TreeNode]
-        :type subRoot: Optional[TreeNode]
-        :rtype: bool
-        """
-        if not root:
-            return False
-        if self.isSameTree(root, subRoot):
-            return True
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-    def isSameTree(self, s, t):
-        if not s and not t:
-            return True
-        if not s or not t or s.val != t.val:
-            return False
-        return self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # 1. Need to identify the where is the head of the subRoot in the tree
+        def sameTree(node1, node2):
+            if not node1 and not node2:
+                return 0
+            if not node1 or not node2:
+                return 1
+
+            if node1.val == node2.val:
+                val = 0
+            else:
+                val = 1
+
+            left = sameTree(node1.left, node2.left)
+            right = sameTree(node1.right, node2.right)
+            
+            return left + right + val 
+
+        def dfs(node1, node2):
+            if not node1:
+                return False
+            if sameTree(node1, node2) == 0:
+                return True
+            else:
+                return dfs(node1.left, node2) or dfs(node1.right, node2)
+        
+        return dfs(root, subRoot)
 
             
-        
+            
+            
