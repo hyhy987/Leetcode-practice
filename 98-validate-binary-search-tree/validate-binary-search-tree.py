56f758dfd0373b9db577d8cce8a5815c94cc7f2a
Hao Yang <132206571+hyhy987@users.noreply.github.com>
2026-09-07 00:08:21 +0800

    Time: 3 ms (34.20%) | Memory: 21 MB (46.37%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -1,24 +1,25 @@
+from collections import deque
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isValidBST(self, root):
-        prev = [float('-inf')]
-        
-        def dfs(node):
-            if node == None:
-                return True
-            if not dfs(node.left):
-                return False
-            if node.val <= prev[0]:
-                return False
 
-            prev[0] = node.val
-            
-            return dfs(node.right)
-    
-        return dfs(root)
-        
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = deque()
+        currMax = float("-inf")
+        curr = root 
+
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop() 
+            if curr.val <= currMax:
+                return False 
+            currMax = curr.val
+            curr = curr.right 
+                
+        return True
